6f05da31b015c21c38ff5c289969d8a62a7169d4
Oscar Cardoso Garcia <75736745+Cardoso-topdev@users.noreply.github.com>
2021-02-27 05:21:50 -0600

    make it better

---
+++ routes/users.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const isAuth = require("../middleware/isAuth");
+const usersController = require("../controllers/users");
+
+const router = express.Router();
+
+const emailValidator = body("email")
+  .isEmail()
+  .normalizeEmail()
+  .withMessage("Email Address is not valid.");
+const passwordValidator = body("password")
+  .trim()
+  .isLength({ min: 6 })
+  .withMessage("Password has to be 6 chars or more.");
+const nameValidator = body("name")
+  .trim()
+  .notEmpty()
+  .withMessage("Name is required.");
+
+// POST /users/signup
+router.post(
+  "/signup",
+  [emailValidator, passwordValidator, nameValidator],
+  usersController.signup
+);
+
+// POST /users/login
+router.post(
+  "/login",
+  [emailValidator, passwordValidator],
+  usersController.login
+);
+
+// POST /users/googlogin
+router.post(
+  "/googlogin",
+  [emailValidator, passwordValidator],
+  usersController.googlogin
+);
+
+// POST /users/logout
+router.post("/logout", isAuth, usersController.logout);
+
+// GET /users/account
+router.get("/account", isAuth, usersController.getUser);
+
+// GET /users/getUserList
+router.get("/getUserList", isAuth, usersController.getUserList);
+
+// PUT /users/account/inbox
+router.put("/account/inbox", isAuth, usersController.updateInbox);
+
+// PUT /users/account
+router.put("/account", isAuth, usersController.updateUser);
+
+// POST /users/resetToken
+router.post("/resetToken", [emailValidator], usersController.getResetToken);
+
+// POST /users/resetPassword
+router.post(
+  "/resetPassword",
+  [passwordValidator],
+  usersController.resetPassword
+);
+
+// POST /users/activate
+router.post("/activate", usersController.activateAccount);
+
+// POST /users/follow
+router.post("/follow", isAuth, usersController.followUser);
+
+// POST /users/follow
+router.post("/saveBioText", usersController.saveBioText);
+
+module.exports = router;
