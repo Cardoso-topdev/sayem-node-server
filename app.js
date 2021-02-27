dbc3c83bb2b20cc9963dfa0c7bf7f9243a6523af
Oscar Cardoso Garcia <75736745+Cardoso-topdev@users.noreply.github.com>
2021-02-27 06:08:42 -0600

    update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "https://readwithmev1.herokuapp.com");
   // res.setHeader("Access-Control-Allow-Origin", "https://readwithmev1.herokuapp.com");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Methods",
