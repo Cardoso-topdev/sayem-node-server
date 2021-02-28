c53914e0c2b4321078cd29d9a85fe8cdc6c3d1f0
Oscar Cardoso Garcia <75736745+Cardoso-topdev@users.noreply.github.com>
2021-02-27 22:01:14 -0600

    change cookie part

--- controllers/users.js
+++ controllers/users.js
@@ -183,14 +183,15 @@ const login = async (req, res, next) => {
     let cookieConfig = ({
         httpOnly: true,
         maxAge: maxAge,
-        domain: process.env.DOMAIN,
+        secure: true
+        // domain: process.env.DOMAIN,
     })
     console.log("==== Cookie =======")
     res.cookie("token", token, cookieConfig);
     res.cookie("userId", user._id.toString(), cookieConfig);
     res.cookie("userName", user.name, cookieConfig);
     console.log("==== end =======")
-    
+
     res.status(201).json({
       message: "User successfully logged in.",
       token: token,
